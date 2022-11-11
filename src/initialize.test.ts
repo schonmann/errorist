8689844ce279ff82cb2ba660976a81ecf2f314a3
Antonio Schönmann <antonio_schonmann@hotmail.com>
2022-11-11 00:06:27 -0300

    test: better description, remove unused import

--- src/initialize.test.ts
+++ src/initialize.test.ts
@@ -1,9 +1,8 @@
 import initializeErrorist from '.';
-import { Config } from './types';
 
 describe('initializeErrorist', () => {
-  test('()', () => {
-    const errorist = initializeErrorist({} as Config);
+  it('should return a defined instance of `initializeErrorist`', () => {
+    const errorist = initializeErrorist();
     expect(errorist).toBeDefined();
   });
 });
